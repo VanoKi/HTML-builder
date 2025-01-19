4d5e3626aa5e748bfdc0842d4c53ec43b1e0dcb1
VanoKi <kirsvano@gmail.com>
2025-01-19 22:03:21 +0300

    feat: add readFile

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,4 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = path.join(__dirname, 'text.txt')
-console.log(filePath);
+// console.log(filePath);
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) console.error(err)
+  else console.log(data)
+})
