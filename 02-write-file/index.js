4e15008c26d2c70376361bacd547b70090f475dc
VanoKi <kirsvano@gmail.com>
2025-01-20 17:25:51 +0300

    feat: learn process

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,14 +2,18 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = 'userInput.txt'
-process.stdout.write('Please, enter the text: ')
-process.stdin.on('data', (data) => {
-  const userInput = data.toString().trim()
-  console.log(`you entered: ${userInput}`);
-  process.exit()
-})
+// process.stdout.write('Please, enter the text: ')
+// process.stdin.on('data', (data) => {
+//   const userInput = data.toString().trim()
+//   console.log(`you entered: ${userInput}`);
+//   process.exit()
+// })
 
 // fs.writeFile(filePath, userInput, (err) => {
 //   if (err) console.error(err);
 //   console.log('file created');
 // })
+
+// console.log(process.env);
+// console.log(process.env.Path);
+console.log(process.cwd());
