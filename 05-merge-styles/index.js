490acd884c50151b5a13d03648c56441c331d595
VanoKi <kirsvano@gmail.com>
2025-01-21 01:06:03 +0300

    feat: create mergeCss

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,3 +4,15 @@ const path = require('path')
 const dirPath = path.join(__dirname, 'styles')
 const outputFile = path.join(__dirname, 'project-dist', 'bundle.css')
 // console.log(outputFile);
+async function mergeCss() {
+  const files = await fs.readdir(dirPath)
+  const cssFiles = files.filter(file => path.extname(file) === '.css')
+  await fs.writeFile(outputFile, '', 'utf-8')
+  for (const cssFile of cssFiles) {
+    const filePath = path.join(dirPath, cssFile)
+    const styles = await fs.readFile(filePath, 'utf-8')
+    await fs.appendFile(outputFile, `/* ${cssFile} */\n${styles}\n`, 'utf-8')
+  }
+}
+
+mergeCss()
