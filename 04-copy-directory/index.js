51ac473c490ff8b1e77bf5248b6870caa9f7805c
VanoKi <kirsvano@gmail.com>
2025-01-20 23:31:06 +0300

    feat: add copDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,2 +1,22 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
+
+const dirPath = path.join(__dirname, 'files')
+const destination = `${dirPath}-copy`
+
+async function copyDir() {
+  try {
+    const files = await fs.readdir(dirPath)
+    await fs.mkdir(destination, {recursive: true})
+    for (const file of files) {
+      const sourceFile = path.join(dirPath, file)
+      const destinationFile = path.join(destination, file);
+      await fs.copyFile(sourceFile, destinationFile)
+      console.log(`file ${file} copied`);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+copyDir()
