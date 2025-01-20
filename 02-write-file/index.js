53d0364c6fb3328ea356e6c56577b7b32394b5cc
VanoKi <kirsvano@gmail.com>
2025-01-20 16:51:39 +0300

    feat: use process

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,8 +2,14 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = 'userInput.txt'
-const userInput = 'hello, vasya!'
-fs.writeFile(filePath, userInput, (err) => {
-  if (err) console.error(err);
-  console.log('file created');
+process.stdout.write('Please, enter the text: ')
+process.stdin.on('data', (data) => {
+  const userInput = data.toString().trim()
+  console.log(`you entered: ${userInput}`);
+  process.exit()
 })
+
+// fs.writeFile(filePath, userInput, (err) => {
+//   if (err) console.error(err);
+//   console.log('file created');
+// })
