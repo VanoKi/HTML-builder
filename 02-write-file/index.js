6a99888c77b5234f9560326e8e758344d8372372
VanoKi <kirsvano@gmail.com>
2025-01-20 19:20:37 +0300

    fix: delete comments in second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,23 @@
 const fs = require('fs')
-const path = require('path')
-
-const filePath = 'userInput.txt'
-const writeStream = fs.createWriteStream(filePath)
+const writeStream = fs.createWriteStream('userInput.txt')
+const farewell = 'input completed'
 process.stdout.write('Please, enter the text: ')
 process.stdin.setEncoding('utf-8')
 process.stdin.on('data', (data) => {
   const input = data.toString().trim()
-  if (input === 'exit') process.exit()
-  // fs.createWriteStream()
+  if (input === 'exit') {
+    writeStream.end()
+    process.exit(0)
+  }
   else writeStream.write(input + '\n')
 })
 
 process.on('SIGINT', () => {
-  process.exit()
+  process.stdout.write(farewell)
+  process.exit(0)
 })
 process.on('exit', () => {
-  process.stdout.write('input completed')
+  process.stdout.write(farewell)
   }
 )
 
