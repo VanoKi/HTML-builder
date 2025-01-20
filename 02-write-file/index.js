e4750aebea860142b35416ca2261d09f07633865
VanoKi <kirsvano@gmail.com>
2025-01-20 18:14:43 +0300

    feat: use exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,23 +2,15 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = 'userInput.txt'
-// process.stdout.write('Please, enter the text: ')
-// process.stdin.on('data', (data) => {
-//   const userInput = data.toString().trim()
-//   console.log(`you entered: ${userInput}`);
-//   process.exit()
-// })
+process.stdout.write('Please, enter the text: ')
+process.stdin.setEncoding('utf-8')
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim()
+  console.log(`your entered: ${data}`);
+  process.exit()
+})
 
 // fs.writeFile(filePath, userInput, (err) => {
 //   if (err) console.error(err);
 //   console.log('file created');
-// })
-
-// console.log(process.env);
-// console.log(process.env.Path);
-console.log(process.cwd());
-console.log(process.platform);
-console.log(process.memoryUsage());
-console.log(process.execPath);
-console.log(process.pid);
-console.log(process.ppid);
+// }
