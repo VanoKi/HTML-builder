9ec268f439ac6bbf7364b9431e38ecbd4d4246f2
VanoKi <kirsvano@gmail.com>
2025-01-20 20:36:06 +0300

    feat: readdir

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,19 @@
 const fs = require('fs')
 const path = require('path')
-const scriptPath = process.cwd()
-console.log(scriptPath);
+const dirPath = path.join(__dirname, 'secret-folder')
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((file) => {
+    // let name = path.parse(file).name
+    let name = path.basename(file, path.extname(file))
+    let ext = path.extname(file)
+    const pathToFile = path.join(dirPath, file)
+    // console.log(pathToFile);
+    fs.stat(pathToFile, (err, stats) => {
+      if (err) console.error(err);
+      let size = (stats.size / 1024).toFixed(2);
+      if (stats.isFile()) {
+        console.log(`${name}-${ext.replace('.', '')}-${size}kb`);
+      }
+    })
+  })
+})
