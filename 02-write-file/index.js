79dfe7f99b2512b7183fa5de7172aec8572685df
VanoKi <kirsvano@gmail.com>
2025-01-20 19:01:58 +0300

    feat: add witeStream

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,15 +2,21 @@ const fs = require('fs')
 const path = require('path')
 
 const filePath = 'userInput.txt'
+const writeStream = fs.createWriteStream(filePath)
 process.stdout.write('Please, enter the text: ')
 process.stdin.setEncoding('utf-8')
 process.stdin.on('data', (data) => {
   const input = data.toString().trim()
-  console.log(`your entered: ${data}`);
-  process.exit()
+  if (input === 'exit') process.exit()
+  // fs.createWriteStream()
+  else writeStream.write(input + '\n')
 })
 
-// fs.writeFile(filePath, userInput, (err) => {
-//   if (err) console.error(err);
-//   console.log('file created');
-// }
+process.on('SIGINT', () => {
+  process.exit()
+})
+process.on('exit', () => {
+  process.stdout.write('input completed')
+  }
+)
+
