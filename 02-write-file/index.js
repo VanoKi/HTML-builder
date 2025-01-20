fd918658e8abe07ebb0f9c7692da8263938b59cc
VanoKi <kirsvano@gmail.com>
2025-01-20 16:26:47 +0300

    feat: use writeFile

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-const os = require('os')
 
-// console.log(os.EOL);
-// console.log(os.endianness());
-// console.log(os.tmpdir());
-// console.log(os.homedir());
-// console.log(os.freemem());
-// console.log(os.loadavg());
-// console.log(os.totalmem());
-// console.log(os.cpus());
-console.log(os.cpus());
-
+const filePath = 'userInput.txt'
+const userInput = 'hello, vasya!'
+fs.writeFile(filePath, userInput, (err) => {
+  if (err) console.error(err);
+  console.log('file created');
+})
