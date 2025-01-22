e958db235d5602de674b1296968f8ce784161991
VanoKi <kirsvano@gmail.com>
2025-01-22 15:30:19 +0300

    feat: read template file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,34 +1,11 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const path = require('path')
 
-const distPath = path.join(__dirname, 'project-dist')
-
-fs.mkdir(distPath, {recursive: true}, (err) => {
+const pathToDist = path.join(__dirname, 'project-dist')
+// console.log(pathToDist);
+const pathToHtml = path.join(__dirname, 'template.html')
+// console.log(pathToHtml);
+fs.readFile(pathToHtml, 'utf-8', (err, data) => {
   if (err) console.error(err);
+  console.log(data);
 })
-
-const templatePath = path.join(__dirname, 'template.html')
-let templateContent = ''
-
-fs.readFile(templatePath, 'utf-8', (err, data) => {
-  if (err) console.error(err);
-  templateContent = data
-  processTemplate()
-})
-
-const componentsPath = path.join(__dirname, 'components')
-const processTemplate = () => {
-  const tagRegex = /{{\s*([\w-]+)\s*}}/g
-  let match
-  while ((match = tagRegex.exec(templateContent)) !== null) {
-  const tagName = match[1];
-  const componentPath = path.join(componentsPath, `${tagName}.html`);
-  fs.readFile(componentPath, 'utf-8', (err, componentContent) => {
-    if (err) console.error(err);
-    templateContent = templateContent.replace(match[0], componentContent)
-    fs.writeFile(path.join(distPath, 'index.html', templateContent, (err) => {
-      if (err) console.error(err);
-    }
-  })
-  })
-}
