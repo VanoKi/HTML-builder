5147433005b96ab3c515900fac56b0cacb66af63
VanoKi <kirsvano@gmail.com>
2025-01-23 16:43:09 +0300

    feat: use regex to compare files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,7 +5,18 @@ const pathToDist = path.join(__dirname, 'project-dist')
 // console.log(pathToDist);
 const pathToHtml = path.join(__dirname, 'template.html')
 // console.log(pathToHtml);
+
 fs.readFile(pathToHtml, 'utf-8', (err, data) => {
   if (err) console.error(err);
-  console.log(data);
+  const updated = data.split('\n')
+  // console.log(updated);
+  for (const string of updated) {
+    if (string.match(/\{\{.*?\}\}/g)) {
+      console.log(string);
+    }
+  }
+  // console.log(updated.match(/\{\{.*?\}\}/g));
 })
+
+// const stream = fs.createReadStream(pathToHtml, {encoding: 'utf-8'})
+// stream.on('data', chunk => console.log(chunk))
