030e0a0ed02815bf4b81b96666e6dff562a15026
VanoKi <kirsvano@gmail.com>
2025-01-23 16:50:09 +0300

    feat: create a fucntion fo work with components file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,21 +2,27 @@ const fs = require('fs')
 const path = require('path')
 
 const pathToDist = path.join(__dirname, 'project-dist')
-// console.log(pathToDist);
+console.log(pathToDist);
 const pathToHtml = path.join(__dirname, 'template.html')
-// console.log(pathToHtml);
+console.log(pathToHtml);
 
-fs.readFile(pathToHtml, 'utf-8', (err, data) => {
-  if (err) console.error(err);
-  const updated = data.split('\n')
-  // console.log(updated);
-  for (const string of updated) {
-    if (string.match(/\{\{.*?\}\}/g)) {
-      console.log(string);
-    }
-  }
-  // console.log(updated.match(/\{\{.*?\}\}/g));
-})
+// fs.readFile(pathToHtml, 'utf-8', (err, data) => {
+//   if (err) console.error(err);
+//   const updated = data.split('\n')
+//   // console.log(updated);
+//   for (const string of updated) {
+//     if (string.match(/\{\{.*?\}\}/g)) {
+//       console.log(string);
+//     }
+//   }
+// })
 
 // const stream = fs.createReadStream(pathToHtml, {encoding: 'utf-8'})
 // stream.on('data', chunk => console.log(chunk))
+
+function readComponentsFile(component) {
+  const pathTpComponents = path.join(__dirname, 'components', `${component}.html`)
+  return pathTpComponents
+}
+
+console.log(readComponentsFile('footer'));
